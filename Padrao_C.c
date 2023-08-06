895a775cabfbb5a1c225719454d8f85fa85ec775
Thiago Anacleto <thiagoanacletonn3@gmail.com>
2023-08-06 20:25:55 -0300

    Inserindo uma mensagem

--- Padrao_C.c
+++ Padrao_C.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #include <locale.h>
 #include <windows.h>
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
-    setlocale(LC_ALL, "portuguese");
+    setlocale(LC_ALL, "Portuguese_Brasil");
 
-
-
+    printf("Hello World!");
+    printf("\n");
+    
     return 0;
 }
